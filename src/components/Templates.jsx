ec585fb0ea420cf4e7208d3e7afb238257a7bed0
itsoumyasanta16 <soumyasantam@gmail.com>
2022-07-02 18:24:56 +0530

    meme_app

---
+++ src/components/Templates.jsx
@@ -0,0 +1,19 @@
+const Templates = ({templates,setMeme})=>{
+    return (<div className="Templates">
+        {templates.map((template)=>(
+    <div key={template.id} className="template" 
+    onClick={()=>{
+          setMeme(template);
+    
+    }}>
+    <div style={{backgroundImage:`url(${template.url})`}} 
+    className="image">
+    </div>
+    </div>
+    
+
+    ))}
+    </div>
+    );
+};
+export default Templates; 
